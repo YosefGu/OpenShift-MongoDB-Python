dd9e5e310f4f4f3402e70c9197701a7c9f8e1655
YosefGu <yos770yos@gmail.com>
2025-08-18 11:56:07 +0300

    add

--- services/dataloader.py
+++ services/dataloader.py
@@ -8,22 +8,10 @@ class DataLoader:
             host=os.getenv("MONGO_HOST"),
             username=os.getenv("MONGO_USER"),
             password=os.getenv("MONGO_PASSWORD"),
+            authSource="admin"
         )
         self.db = self.client[os.getenv("MONGO_DATABASE", "test_db")]
         self.collection = self.db["users"]
 
     def get_data(self):
         return list(self.collection.find({}, {"_id": 0})) 
-
-    def add_data(self, user):
-        try:
-            doc = {
-                "id": user.id,
-                "first_name": user.first_name,
-                "last_name": user.last_name
-            }
-            self.collection.insert_one(doc)
-            return True
-        except Exception as e:
-            print("Error:", e)
-            return e
